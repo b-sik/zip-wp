69d4d9a8e3d946fbd7522f501731350e8d6aaf44
Brian Siklinski <bszyk.codes@gmail.com>
2021-11-09 21:16:14 -0500

    update .sh path

--- src/ZipWP/ZipWP.php
+++ src/ZipWP/ZipWP.php
@@ -5,6 +5,6 @@ namespace ZipWP;
 class ZipWP {
 
 	public static function run_script() {
-		echo shell_exec( 'bash ./vendor/zip-wp/src/ZipWP/zip.sh' ); // phpcs:ignore
+		echo shell_exec( 'bash ./vendor/bszyk/zip-wp/src/ZipWP/zip.sh' ); // phpcs:ignore
 	}
 }
