49c1635c5da58ca806f2b2b067491ddbdc7e59be
Brian Siklinski <bszyk.codes@gmail.com>
2021-11-09 21:23:23 -0500

    include vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'v0.1.0-alpha',
+        'version' => '0.1.0.0-alpha',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'bszyk/zip-wp',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'bszyk/zip-wp' => array(
+            'pretty_version' => 'v0.1.0-alpha',
+            'version' => '0.1.0.0-alpha',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
